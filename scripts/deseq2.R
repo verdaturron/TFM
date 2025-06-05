33c9b95ca51253588c50b6b810e625179c007403
verdaturron <fandemontagne@gmail.com>
2025-06-05 14:26:19 +0200

    Update deseq2.R

--- scripts/deseq2.R
+++ scripts/deseq2.R
@@ -1,26 +1,26 @@
 #!/usr/bin/env Rscript
 suppressPackageStartupMessages({
   library(DESeq2)
-  library(readr)   # vroom
+  library(readr)   
   library(dplyr)
 })
 
-## ── chemins fournis par Snakemake ──────────────────────────
+# chemins fournis par Snakemake
 counts_file <- snakemake@input[["counts"]]
 meta_file   <- snakemake@input[["meta"]]
 out_table   <- snakemake@output[["table"]]
 out_rds     <- snakemake@output[["rds"]]
 
-## ── lecture des données ────────────────────────────────────
+# lecture des données
 cts  <- read_tsv(counts_file, col_types = cols())
 meta <- read_tsv(meta_file,   col_types = cols()) |>
-  mutate(across(where(is.character), factor))   # caractères → facteurs
+  mutate(across(where(is.character), factor))
 
-# on place les gènes en rownames et on « arrondit » les comptes
+# gènes en rownames
 mat <- as.matrix(round(cts[,-1]))
 rownames(mat) <- cts$gene_id
 
-## ── objet DESeq2 ───────────────────────────────────────────
+#DESeq2
 dds <- DESeqDataSetFromMatrix(mat, colData = meta, design = ~ condition)
 dds <- DESeq(dds)
 
@@ -28,6 +28,6 @@ res <- results(dds) |>
   as_tibble(rownames = "gene_id") |>
   arrange(padj)
 
-## ── sorties ────────────────────────────────────────────────
+# sorties
 write_tsv(res,  out_table)
 saveRDS(dds, out_rds)
